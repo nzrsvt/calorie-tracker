289c584f6176bf486bc1e78ac2f7160c4c482c21
nzrsvt <nsavitsky89@gmail.com>
2024-06-28 14:23:24 +0300

    Create FoodItem and UserMeal models

--- tracker/models.py
+++ tracker/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class FoodItem(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    calories = models.PositiveIntegerField()
+    protein = models.FloatField()
+    fat = models.FloatField()
+    carbohydrates = models.FloatField()
+    portion_size = models.FloatField()
+
+    GRAMS = 'g'
+    MILLILITERS = 'ml'
+    PIECES = 'pcs'
+    UNIT_CHOICES = [
+        (GRAMS, 'Grams'),
+        (MILLILITERS, 'Milliliters'),
+        (PIECES, 'Pieces'),
+    ]
+    
+    quantity_unit = models.CharField(max_length=3, choices=UNIT_CHOICES, default=GRAMS)
+
+class UserMeal(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='meals')
+    food_item = models.ForeignKey(FoodItem, on_delete=models.CASCADE, related_name='meals')
+    datetime = models.DateTimeField(auto_now_add=True)
+
+    quantity = models.FloatField()  
+
+    class Meta:
+        ordering = ['-datetime']
